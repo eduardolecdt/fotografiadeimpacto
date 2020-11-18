69697e0d0a8f4df23192d7583ea37b8d9b35c5f7
Luiz Eduardo <eduardolecdt@gmail.com>
2020-11-18 07:28:36 -0300

    Ajustes

--- assets/js/pages/index/slide-galeria-hover.js
+++ assets/js/pages/index/slide-galeria-hover.js
@@ -1,68 +1,30 @@
-const __slide = document.querySelector('section.galeria .slide .itens .conteudo')
+(function(){
 
-// Imagens
+  // Internal Variables
 
-const __imagemUm = __slide.querySelector('.imagem img.fixa.slide1')
-const __imagemUmZoom = __slide.querySelector('.imagem img.zoom.slide1')
+  const __galeria = document.querySelector('section[name="galeria"]')
+  const __querySlide = 'img[name="slide"]'
+  const __queryZoom = 'img[name="zoom"]'
 
-const __imagemDois = __slide.querySelector('.imagem img.fixa.slide2')
-const __imagemDoisZoom = __slide.querySelector('.imagem img.zoom.slide2')
+  // Methods
 
-const __imagemTres = __slide.querySelector('.imagem img.fixa.slide3')
-const __imagemTresZoom = __slide.querySelector('.imagem img.zoom.slide3')
+  function habilitarMouseOver () {
+    __galeria.addEventListener('mouseover', (evento) => {
+      if (!evento.target.matches(__querySlide)) return
 
-const __imagemQuatro = __slide.querySelector('.imagem img.fixa.slide4')
-const __imagemQuatroZoom = __slide.querySelector('.imagem img.zoom.slide4')
+      const imgZoom = evento.target.parentElement.querySelector('img[name="zoom"]')
+      imgZoom.classList.add('mostrar')
+    })
+  }
 
-const __imagemCinco = __slide.querySelector('.imagem img.fixa.slide5')
-const __imagemCincoZoom = __slide.querySelector('.imagem img.zoom.slide5')
+  function habilitarMouseOut () {
+    __galeria.addEventListener('mouseout', (evento) => {
+      if (!evento.target.matches(__queryZoom)) return
 
-// 1
+      evento.target.classList.remove('mostrar')
+    })
+  }
 
-__imagemUm.onmouseover = function() {
-  __imagemUmZoom.classList.add('mostrar')
-}
-
-__imagemUm.onmouseout = function() {
-  __imagemUmZoom.classList.remove('mostrar')
-}
-
-// 2
-
-__imagemDois.onmouseover = function() {
-  __imagemDoisZoom.classList.add('mostrar')
-}
-
-__imagemDois.onmouseout = function() {
-  __imagemDoisZoom.classList.remove('mostrar')
-}
-
-// 3
-
-__imagemTres.onmouseover = function() {
-  __imagemTresZoom.classList.add('mostrar')
-}
-
-__imagemTres.onmouseout = function() {
-  __imagemTresZoom.classList.remove('mostrar')
-}
-
-// 4
-
-__imagemQuatro.onmouseover = function() {
-  __imagemQuatroZoom.classList.add('mostrar')
-}
-
-__imagemQuatro.onmouseout = function() {
-  __imagemQuatroZoom.classList.remove('mostrar')
-}
-
-// 5
-
-__imagemCinco.onmouseover = function() {
-  __imagemCincoZoom.classList.add('mostrar')
-}
-
-__imagemCinco.onmouseout = function() {
-  __imagemCincoZoom.classList.remove('mostrar')
-}
+  habilitarMouseOver()
+  habilitarMouseOut()
+}())
