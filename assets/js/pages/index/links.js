ee2a28512d420cf21ccf0c5ad29408f71af5e602
Luiz Eduardo <eduardolecdt@gmail.com>
2020-11-18 21:26:06 -0300

    VERSÃO FINAL 1.0

--- assets/js/pages/index/links.js
+++ assets/js/pages/index/links.js
@@ -1,3 +1,7 @@
 // Links
 
-document.querySelector('').href = ''
+document.querySelector('#area_membros').href = 'https://comunidadefotografiadeimpacto.club.hotmart.com/login'
+
+document.querySelector('#teste_gratis').href = 'https://pay.hotmart.com/V43600858V?off=2dn0lubu'
+document.querySelector('#preco_mensal').href = 'https://pay.hotmart.com/V43600858V?off=8zgvs9q6'
+document.querySelector('#preco_anual').href = 'https://pay.hotmart.com/V43600858V?off=c8y7dk5y'
